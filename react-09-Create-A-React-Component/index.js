c4d320d814ce7e1bde2ab45f401d460fefd392bd
Clinton Lunn <cpl09c@gmail.com>
2019-06-16 13:09:44 -0400

    initial question

---
+++ react-09-Create-A-React-Component/index.js
@@ -0,0 +1,13 @@
+
+class MyComponent extends React.Component {
+    constructor(props) {
+      super(props);
+    }
+    render() {
+      // change code below this line
+  
+  
+  
+      // change code above this line
+    }
+  };

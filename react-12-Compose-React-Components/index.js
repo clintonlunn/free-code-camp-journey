c364b4b0c9fdf3926762754d312eade42df3ba5d
Clinton Lunn <cpl09c@gmail.com>
2019-06-16 14:05:12 -0400

    final solution

--- react-12-Compose-React-Components/index.js
+++ react-12-Compose-React-Components/index.js
@@ -1,33 +1,21 @@
-class Fruits extends React.Component {
+
+class TypesOfFood extends React.Component {
     constructor(props) {
       super(props);
     }
     render() {
       return (
         <div>
-          <h2>Fruits:</h2>
-          { /* change code below this line */ }
-            <NonCitrus />
-            <Citrus />
-           { /* change code above this line */ }
+          <h1>Types of Food:</h1>
+          {/* change code below this line */}
+          <Fruits />
+          <Vegetables />
+          {/* change code above this line */}
         </div>
       );
     }
   };
   
-  class TypesOfFood extends React.Component {
-    constructor(props) {
-       super(props);
-    }
-    render() {
-      return (
-        <div>
-          <h1>Types of Food:</h1>
-          { /* change code below this line */ }
-          <Fruits />
-          { /* change code above this line */ }
-          <Vegetables />
-        </div>
-      );
-    }
-  };
+  const challengeNode =  document.getElementById('challenge-node');
+  // change code below this line
+  ReactDOM.render(<TypesOfFood />, challengeNode);

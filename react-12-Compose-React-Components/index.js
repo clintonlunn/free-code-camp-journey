ac8d57623a77ff52dfbc2e7345d80ee58b1c9567
Clinton Lunn <cpl09c@gmail.com>
2019-06-16 13:42:10 -0400

    final solution

--- react-12-Compose-React-Components/index.js
+++ react-12-Compose-React-Components/index.js
@@ -7,7 +7,8 @@ class Fruits extends React.Component {
         <div>
           <h2>Fruits:</h2>
           { /* change code below this line */ }
-    
+            <NonCitrus />
+            <Citrus />
            { /* change code above this line */ }
         </div>
       );
@@ -23,7 +24,7 @@ class Fruits extends React.Component {
         <div>
           <h1>Types of Food:</h1>
           { /* change code below this line */ }
-  
+          <Fruits />
           { /* change code above this line */ }
           <Vegetables />
         </div>

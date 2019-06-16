00b778eebed30de26c38893381fe11e667011edd
Clinton Lunn <cpl09c@gmail.com>
2019-06-16 14:12:53 -0400

    final solution

--- react-13-Write-a-React-Component-From-Scratch/index.js
+++ react-13-Write-a-React-Component-From-Scratch/index.js
@@ -1,1 +1,16 @@
 // change code below this line
+class MyComponent extends React.Component {
+    constructor(props) {
+      super(props)
+    }
+    render () {
+      return (
+        <div>
+          <h1>My First React Component!</h1>
+        </div>
+      )
+    }
+  }
+  
+  const challengeNode = document.getElementById('challenge-node');
+  ReactDOM.render(<MyComponent />, challengeNode)
